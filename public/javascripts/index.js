5c00c293e9a976d04403c7ac89ec7951d8809e02
MinGyu, Ju <knighp@nate.com>
2017-07-15 22:39:11 +0900

    back location.host

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
     options: basicOption
   });
 
-  var ws = new WebSocket('wss://' + jmghub.azure-devices.net);
+  var ws = new WebSocket('wss://' + location.host);
   ws.onopen = function () {
     console.log('Successfully connect WebSocket');
   }
